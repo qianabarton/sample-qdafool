d2d1038ea2df81fe0fd0432e88ec9958aca457f0
QIANA <barton.qiana@gmail.com>
2018-02-11 23:35:43 -0800

    Navbar Fixed

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,11 +1,32 @@
+function menuSlide() {
+  console.log("HIHIHI");
+  var $toggle = document.getElementById('menu-toggle');
+  var $sideMenu = document.getElementById('side-menu');
+  var $slidecontent = document.getElementById('slide-content');
+  var $navbar = document.getElementById('navbar')
+
+  $toggle.addEventListener('click', function(){
+    $sideMenu.classList.toggle("sideMenuToggle");
+    $slidecontent.classList.toggle("slideOver");
+    $navbar.classList.toggle("nav-slide");
+
+  });
+}
+
+function transparentNav(){
+  // Transition effect for navbar
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 450px, adds/removes solid class
+    if($(this).scrollTop() > 450) {
+        $('.navbar').addClass('solid');
+    } else {
+        $('.navbar').removeClass('solid');
+    }
+  });
+}
+
+
 $(document).ready(function() {
-        // Transition effect for navbar
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 500) {
-              $('.navbar').addClass('solid');
-          } else {
-              $('.navbar').removeClass('solid');
-          }
-        });
+  menuSlide();
+  transparentNav();
 });
